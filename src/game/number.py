e4db012275545bc37920eaa05f4a629e2be4a78e
jjjosippp <104211715+jjjosippp@users.noreply.github.com>
2022-06-26 13:42:47 +0200

    make number have colour

--- src/game/number.py
+++ src/game/number.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+from typing import List
 
 TABLE_WIDTH = 3
 MAX_NUMBER = 36
@@ -11,6 +12,17 @@ class Colour(Enum):
 class Number:
   def __init__(self, n: int) -> None:
     self.n: int = n
+    self. colour = Colour.getColour(n)
+    
+  @staticmethod
+  def getColour(num: int) -> Colour:
+    reds: List[int] = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+    if num == 0:
+      return Colour.GREEN
+    elif num in reds:
+      return Colour.RED
+    else:
+      return Colour.BLACK
   
   @staticmethod
   def x(n: int) -> int: return (n - 1) % 3
