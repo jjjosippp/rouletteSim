588c68b3e322690747101b04783aa0cfd6a5d329
jjjosippp <104211715+jjjosippp@users.noreply.github.com>
2022-06-26 12:42:19 +0200

    Add Money class

--- src/game/play.py
+++ src/game/play.py
@@ -2,6 +2,13 @@ from random import Random
 from dataclasses import dataclass
 
 @dataclass
+class Money:
+  pence: int
+  
+  def pounds(self) -> float:
+    self.pence / 100
+
+@dataclass
 class Player:
   name: str
 
