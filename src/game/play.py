e555605fd1995baea7263b9e64aae8e509ca3b18
jjjosippp <104211715+jjjosippp@users.noreply.github.com>
2022-06-26 15:28:53 +0200

    add rollnumber function

--- src/game/play.py
+++ src/game/play.py
@@ -1,7 +1,7 @@
 from random import Random
 from typing import List, Tuple
 from bet import Bet, Money
-from number import Number
+from number import Number, MAX_NUMBER
 from dataclasses import dataclass
 
 @dataclass
@@ -14,3 +14,6 @@ class Play:
     
   def playRound(self, bets: List[Tuple[Player, Bet]]) -> List[Tuple[Player, Money]]:
     pass
+  
+  def rollNumber(self) -> Number:
+    return Number(self.random.randint(0, MAX_NUMBER))
