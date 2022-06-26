269fbc14f21a7959020b4f755df8169a5f11c87c
jjjosippp <104211715+jjjosippp@users.noreply.github.com>
2022-06-26 13:35:34 +0200

    add BetTypes enum

--- src/game/bet.py
+++ src/game/bet.py
@@ -1,4 +1,20 @@
+from enum import Enum, auto
 
+class BetType(Enum):
+  STRAIGHT_UP = auto()
+  SPLIT = auto()
+  STREET = auto()
+  CORNER = auto()
+  BASKET = auto()
+  LINE = auto()
+  DOZEN = auto()
+  COLUMN = auto()
+  EVEN = auto()
+  ODD = auto()
+  RED = auto()
+  BLACK = auto()
+  LOW = auto()
+  HIGH = auto()
 
 class Bet:
   pass
